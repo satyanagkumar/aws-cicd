c3f6c940c60c09cc6f7426efbb834abfe86d5993
satyanagkumar <152373497+satyanagkumar@users.noreply.github.com>
2025-04-04 14:05:00 -0500

    add the files for codedeploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull nagendra2001/simple-python-flask-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 nagendra2001/simple-python-flask-app
